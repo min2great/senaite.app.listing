bc330ce184b45b240ca8e8b231e39fed942c9ccf
Valerio Zhang <valeriozhang14@gmail.com>
2021-02-19 19:43:33 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-version = "1.5.3rc9"
+version = "1.5.3rc10"
 
 with open("docs/About.rst", "r") as fh:
     long_description = fh.read()
@@ -39,7 +39,7 @@ setup(
     zip_safe=False,
     install_requires=[
         "setuptools",
-        "valer.core==1.3.5rc9",
+        "valer.core==1.3.5rc10",
     ],
     extras_require={
         "test": [
